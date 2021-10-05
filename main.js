d8dc2845db8e185dc48ddefc93abf907884041bc
boatengdickson <dicksonboateng67@gmail.com>
2021-10-05 04:29:25 +0000

    Build "about" component

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__menu");
+const mainMenu = document.querySelector(".main");
 
 hamburger.addEventListener("click", (event) => {
     navMenu.classList.toggle("active");
     hamburger.classList.toggle("active");
+    // mainMenu.classList.toggle('lock-scroll');
 })
