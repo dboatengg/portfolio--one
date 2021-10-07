45b89140c8780330ac6a83fe64e473db72e3e1d8
boatengdickson <dicksonboateng67@gmail.com>
2021-10-07 02:37:41 +0000

    close nav menu close button when nav link is clicked

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__menu");
 const mainMenu = document.querySelector(".main");
+const navLinks = document.querySelectorAll(".nav__link");
 
-hamburger.addEventListener("click", (event) => {
+hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
     hamburger.classList.toggle("active");
-    // mainMenu.classList.toggle('lock-scroll');
-})
+});
+
+//remove navmenu and button close after a button is clicked
+navLinks.forEach(link => link.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
